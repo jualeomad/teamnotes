ec8b3f25413edc9174a6c7105b2768ac492000cb
rafgargal <rafgargal@alum.us.es>
2024-05-19 14:01:17 +0200

    fix: mejora custom user

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,30 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'equipo')
+        fields = ('email', 'username', 'team')
 
 class CustomUserLoginForm(AuthenticationForm):
-    username = forms.EmailField(widget=forms.EmailInput(attrs={'autofocus': True}))
+    username = forms.CharField(max_length=30, label="Username or Email")
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username is not None and password:
+            user = CustomUser.objects.filter(email=username).first()
+
+            if user is None:
+                user = CustomUser.objects.filter(username=username).first()
+
+            if user is None or not user.check_password(password):
+                raise forms.ValidationError(
+                    "Invalid username or password. Please try again."
+                )
+            self.user_cache = user
+
+        return self.cleaned_data
--- accounts/models.py
+++ accounts/models.py
@@ -1,39 +1,38 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, equipo, password=None, **extra_fields):
+    def create_user(self, email, username, team, password=None, **extra_fields):
         if not email:
-            raise ValueError(_('The Email field must be set'))
+            raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, equipo=equipo, **extra_fields)
+        user = self.model(email=email, username=username, team=team, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, equipo, password=None, **extra_fields):
+    def create_superuser(self, email, username, team, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
+            raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
+            raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, equipo, password, **extra_fields)
+        return self.create_user(email, username, team, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
-    equipo = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    team = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['equipo']
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'team']
 
     def __str__(self):
         return self.email
