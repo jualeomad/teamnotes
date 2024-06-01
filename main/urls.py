cf9b7bd14a00767d8daab965630114facdae7ab5
rafgargal <rafgargal@alum.us.es>
2024-06-01 13:35:05 +0200

    fix: rutas

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('create_note/', views.create_note_view, name='create_note'),
     path('edit_note/<str:note_id>/', views.edit_note, name='edit_note'),
     path('delete_note/<str:note_id>/', views.delete_note, name='delete_note'),
