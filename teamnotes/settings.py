01c8d238706b8fefd17ddb411ff94848b2173ce4
jualeomad <jualeomad@alum.us.es>
2024-07-01 18:59:31 +0200

    config: deploy

--- teamnotes/settings.py
+++ teamnotes/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!r0s%t*&tj48qo!)lzx)y!7_m69xh_837hl6b6ct#-n=r6c_-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*'] if DEBUG else ['*.onrender.com']
+ALLOWED_HOSTS = ['*']
 if not DEBUG:
     CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
 
@@ -93,8 +93,8 @@ DATABASES = {
 
 COUCHDB_DATABASE_NAME = 'teamnotesdb'
 COUCHDB_SERVER_URL = 'http://' + os.getenv('COUCHDB_USER') + ':' + os.getenv('COUCHDB_PASSWORD') + '@localhost:5984'
-# if not DEBUG:
-COUCHDB_SERVER_URL = os.getenv('COUCHDB_REMOTE_SERVER')
+if not DEBUG:
+    COUCHDB_SERVER_URL = os.getenv('COUCHDB_REMOTE_SERVER')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
